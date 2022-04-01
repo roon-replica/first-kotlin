fede1018a8b81197087873b7e52ba65f3a396e71
leemr <think-roon@naver.com>
2022-04-01 23:03:54 +0900

    [example] hello world

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,9 @@
+// kotlin file
+// entry point is 'main' function, 'main' returns nothing
+// semicolon is optional
+// println is imported implicitly
+// version >= 1.3 can skip parameters
+
+fun main(){
+    println("hello world!")
+}
